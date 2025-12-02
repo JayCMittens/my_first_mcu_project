487bc54a5ad65f2b94e3551fe91cf5fb167d9719
JayCMittens <ctuckett93@gmail.com>
2025-12-02 08:29:46 -0700

    setup linker more

--- src/startup_stm32f303.rs
+++ src/startup_stm32f303.rs
@@ -1,10 +1,13 @@
 // define the vector table tfor the mcu
 
 // define the reset handler
+#[unsafe(no_mangle)]
 fn reset_handler() {
   // copy the .data section from FLASH to RAM
   // zero out the .bss section in the RAM
   // call main()
+
+  crate::main();
 }
 
 // define the exception handlers
